148997b87c233710b5878024be477f0d40be7a12
DebaPrasadbehura424 <debaprasadbehura89@gmail.com>
2026-08-01 14:41:52 +0530

    ic

---
+++ main.py
@@ -0,0 +1,38 @@
+a=20
+b=10
+
+# arithmatic 
+print(a+b);
+print(a-b);
+print(a*b);
+print(a/b);
+print(a//b);
+print(a%b);
+print(a**b);
+
+# relational
+
+print(a<b);
+print(a>b);
+print(a>=b);
+print(a==b);
+print(a!=b);
+
+# Logical
+print(a and b)
+print(a or b)
+print(not a)
+
+
+# Assignment
+a+=1;
+print(a)
+a-=1;
+print(a)
+a*=1;
+print(a)
+a/=1;
+print(a)
+
+
+

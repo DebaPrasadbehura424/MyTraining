1445a0982096ea3b26db4cbf37274ede24d2d0ad
DebaPrasadbehura424 <debaprasadbehura89@gmail.com>
2026-08-01 15:01:22 +0530

    ic

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
-a=20
-b=10
+# a=20
+# b=10
 
-# arithmatic 
-print(a+b);
-print(a-b);
-print(a*b);
-print(a/b);
-print(a//b);
-print(a%b);
-print(a**b);
+# # arithmatic 
+# print(a+b);
+# print(a-b);
+# print(a*b);
+# print(a/b);
+# print(a//b);
+# print(a%b);
+# print(a**b);
 
-# relational
+# # relational
 
-print(a<b);
-print(a>b);
-print(a>=b);
-print(a==b);
-print(a!=b);
+# print(a<b);
+# print(a>b);
+# # print(a>=b);
+# print(a==b);
+# print(a!=b);
 
-# Logical
-print(a and b)
-print(a or b)
-print(not a)
+# # Logical
+# print(a and b)
+# print(a or b)
+# print(not a)
 
 
-# Assignment
-a+=1;
-print(a)
-a-=1;
-print(a)
-a*=1;
-print(a)
-a/=1;
-print(a)
+# # Assignment
+# a+=1;
+# print(a)
+# a-=1;
+# print(a)
+# a*=1;
+# print(a)
+# a/=1;
+# print(a)
+
+str= "Hello Buddy!"
+# print(str)
+
+print(str[0:6])
+print(str[6:])
+print(str.endswith('x')) # false
+print(str.endswith('!')) # true
+
+
 
 
 

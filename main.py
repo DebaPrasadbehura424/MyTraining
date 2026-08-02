8747f89edaa710bbb7d465199bc8f5c5d638f3fd
DebaPrasadbehura424 <debaprasadbehura89@gmail.com>
2026-08-02 08:54:54 +0530

    update

--- main.py
+++ main.py
@@ -34,13 +34,35 @@
 # a/=1;
 # print(a)
 
-str= "Hello Buddy!"
+# str= "Hello Buddy!"
 # print(str)
 
-print(str[0:6])
-print(str[6:])
-print(str.endswith('x')) # false
-print(str.endswith('!')) # true
+# print(str[0:6])
+# print(str[6:])
+# print(str.endswith('x')) # false
+# print(str.endswith('!')) # true
+# print("manas sir".capitalize())
+# print("manas sir".lower())
+# print("manas sir".upper())
+
+# print("manas sir".replace("s","v"))
+# print("manas sir".find("s"))
+# print("manass sir".count("s"))
+
+
+# age = 20;
+
+# if age>18:
+#     print("adult")
+# elif age <18:
+#     print("minor")
+# else:    
+#     print("age is equal to 18")
+
+
+
+
+
 
 
 

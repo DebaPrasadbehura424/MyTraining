52ec2db31dd600867d9552bf0788169dbbb6be5e
DebaPrasadbehura424 <debaprasadbehura89@gmail.com>
2026-08-05 18:19:26 +0530

    add update

--- index.py
+++ index.py
@@ -4,5 +4,15 @@ data=pd.read_csv("C:/Users/debap/OneDrive/Desktop/class/data.csv")
 df=pd.DataFrame(data)
 desc=df.describe()
 h=df.head(1)
+t=df.tail()
+
+
 # print(desc)
-print(h)
+# print(h)
+# print(t)
+
+# print(df.info())
+
+print(df.loc[0])
+print("break")
+print(df.iloc[1])
